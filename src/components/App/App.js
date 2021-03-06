b3b3308ef33e005cb208a29b0002504e2dc36681
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-05 20:46:22 -0700

    Style Header for position

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from '../Header/Header';
 import Aside from '../Aside/Aside';
 import Recipes from '../Recipes/Recipes'
-import 'App.css'
+import './App.css'
 
 const App = () => {
   return (
