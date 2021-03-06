38fcc9f8702bb10ac7c56051d6f886fcde9e2af1
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-05 20:35:38 -0700

    Add additional components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+import Header from '../Header/Header';
+import Aside from '../Aside/Aside';
+import Recipes from '../Recipes/Recipes'
+import 'App.css'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Aside />
+      <Recipes />
+    </>
+  )
+}
 
 export default App
