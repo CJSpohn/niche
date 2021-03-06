ce2569b7182753da0808ba3daf2059467bf16b5b
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-05 20:40:31 -0700

    Add Aside placeholder

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Aside.css';
 
-const Aside
+const Aside = () => {
+  return (
+    <section className="aside">
+      <h1>Aside</h1>
+    </section>
+  )
+}
 
 export default Aside;
