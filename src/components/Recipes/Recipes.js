7cc8e388537056b891717af68da7ee8bd6c68a71
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-05 20:42:55 -0700

    Add Recipes placeholder

---
+++ src/components/Recipes/Recipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Recipes.css'
+
+const Recipes = () => {
+  return(
+    <section className="recipes">
+      <h1>Recipes</h1>
+    </section>
+  )
+}
+
+export default Recipes;
