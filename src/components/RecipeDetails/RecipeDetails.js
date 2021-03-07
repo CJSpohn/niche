ff0e501b53049b765818b32ad4556ed6972d786c
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-07 10:31:21 -0700

    Add missing ingredients list to recipe details

--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -3,15 +3,25 @@ import { Link } from 'react-router-dom';
 import './RecipeDetails.css'
 
 const RecipeDetails = ({ currentRecipe, setCurrentRecipe }) => {
+  console.log(currentRecipe)
+  const missingIngredients = currentRecipe.missedIngredients.map(ingr => ingr.original);
+  const missingIngredientsComponents = missingIngredients.map((ingr, index) => <li key={index}>{ingr}</li>);
   return (
-    <>
+    <section className="details">
       <h1 className="recipe-title">{currentRecipe.title}</h1>
+      <div className="details-wrapper">
+        <img className="recipe-image" src={currentRecipe.image} alt={`${currentRecipe.title}`}/>
+        <h4>You are missing:</h4>
+        <ul>
+          {missingIngredientsComponents}
+        </ul>
+      </div>
       <Link to={{
         pathname:`/`
       }}>
         <button onClick={() => setCurrentRecipe([])} className="back-btn">Back to results</button>
       </Link>
-    </>
+    </section>
   )
 }
 
