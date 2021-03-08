ea805d3fc3c59fc9fdd9c91d211e129be7b7c30b
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-08 11:21:57 -0700

    Add test for existance of everything on home page

--- cypress/integration/home-spec.js
+++ cypress/integration/home-spec.js
@@ -4,11 +4,36 @@ describe ('Home Page', () => {
   })
 
   it('should have a header', () => {
-    cy.get('.header')
+    cy.get('header')
       .contains('Kitchen Sink')
 
     cy.get('h1')
       .get('p')
       .contains('Use it before you lose it')
+  })
+
+  it('should have a current highlights section', () => {
+    cy.get('.aside')
+      .contains('Current Highlights')
+  })
+
+  it('the highlights should show that nothing is there initially', () => {
+    cy.get('.aside')
+      .get('p')
+      .contains('You haven\'t favorited anything yet')
+  })
+
+  it('should contain instructions', () => {
+    cy.get('.recipes')
+      .get('h1')
+      .contains('What ingredients do you need to use?')
+  })
+
+  it('should have a form input and a button', () => {
+    cy.get('form')
+      .should('exist')
+
+    cy.get('button')
+      .should('exist')
   })
 });
